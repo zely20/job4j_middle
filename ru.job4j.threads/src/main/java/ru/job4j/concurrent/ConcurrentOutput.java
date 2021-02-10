09bd61e10f62c417a7c1817748db7feea4de0756
zely20@mail.ru <Civik1984>
2021-02-10 18:59:11 +0300

    add ConcurrentOutput

---
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,11 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+        System.out.println(Thread.currentThread().getName());
+    }
+}
