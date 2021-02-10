48042c2b821d9526c84d82f4f075ba64e26ffccb
zely20@mail.ru <Civik1984>
2021-02-10 20:01:43 +0300

    1.2. Режим ожидания.[231217#471941]

---
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,15 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <=100; i++ ) {
+            System.out.print("\rLoading : " + i  + "%");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
