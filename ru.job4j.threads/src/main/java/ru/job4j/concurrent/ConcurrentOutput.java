f8e62f40b33bf6bcf5ee1a6db1a5454a2db3eb86
zely20@mail.ru <Civik1984>
2021-02-10 19:35:42 +0300

    add second method

--- ru.job4j.threads/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -5,7 +5,13 @@ public class ConcurrentOutput {
         Thread another = new Thread(
                 () -> System.out.println(Thread.currentThread().getName())
         );
+
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+
         another.start();
+        second.start();
         System.out.println(Thread.currentThread().getName());
     }
 }
