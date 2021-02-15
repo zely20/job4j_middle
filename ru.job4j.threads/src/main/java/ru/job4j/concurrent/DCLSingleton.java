460e2d0ade8aba536e64a0d312ab3c1c22b26394
Aliaksandr Zelianko <zely20@mail.ru>
2021-02-15 15:26:37 +0300

    2. Модель памяти Java[267917#471947]

---
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/DCLSingleton.java
@@ -0,0 +1,17 @@
+package ru.job4j.concurrent;
+
+public final class DCLSingleton {
+    private static volatile DCLSingleton inst;
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+    private DCLSingleton() {
+    }
+}
