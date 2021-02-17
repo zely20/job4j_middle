11c7399e08ae8b881f7589db9073ebb0e1867153
Aliaksandr Zelianko <zely20@mail.ru>
2021-02-17 13:23:23 +0300

    промежуточный комит

--- ru.job4j.threads/src/main/java/ru/job4j/concurrent/ParseFile.java
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/ParseFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.concurrent;
 
 import java.io.*;
+import java.util.function.Predicate;
 
 public class ParseFile {
 
@@ -13,6 +14,20 @@ public class ParseFile {
     public synchronized File getFile() {
         return file;
     }
+
+    public synchronized String getContent(Predicate<Integer> pred) throws IOException {
+        String output = "";
+        int data;
+        try (InputStream i = new FileInputStream(file)) {
+            while ((data = i.read()) > 0) {
+                if (pred.test(0x80)) {
+                    output += (char) data;
+                }
+            }
+        }
+        return output;
+    }
+
 
     public synchronized String getContent() throws IOException {
         String output = "";
@@ -40,7 +55,7 @@ public class ParseFile {
 
     public synchronized void saveContent(String content) throws IOException {
         try (OutputStream o = new FileOutputStream(file)) {
-            for (int i = 0; i < content.length(); i += 1) {
+            for (int i = 0; i < content.length(); i ++) {
                 o.write(content.charAt(i));
             }
         }
