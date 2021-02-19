0e0f23be142d6a9ae65d96bfe6f17d89bc57f6e1
Aliaksandr Zelianko <zely20@mail.ru>
2021-02-19 13:07:25 +0300

    refactoring UserStorage.java

--- ru.job4j.threads/src/main/java/ru/job4j/concurrent/UserStorage.java
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/UserStorage.java
@@ -2,44 +2,38 @@ package ru.job4j.concurrent;
 
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.HashMap;
+import java.util.Map;
 
 @ThreadSafe
 public class UserStorage {
 
-    Set<User2> users = new HashSet<>();
+    final Map<Integer, User2> users = new HashMap<>();
 
     public synchronized boolean add (User2 user){
         if(user != null){
-            users.add(user);
+            users.put(user.getId(),user);
             return true;
         }
         return false;
     }
     public synchronized boolean update(User2 user){
-        if(users.contains(user)){
-            users.add(user);
+        if(users.containsKey(user.getId())){
+            users.put(user.getId(),user);
             return true;
         }
         return false;
     }
     public synchronized boolean delete(User2 user){
-        if (users.contains(user)) {
-            users.remove(user);
+        if (users.containsKey(user.getId())) {
+            users.remove(user.getId());
             return true;
         }
         return false;
     }
     public synchronized void transfer(int fromId, int toId, int amount){
-        User2 userTempFirst = users.stream()
-                .filter(user -> user.getId() == fromId)
-                .findFirst()
-                .get();
-        User2 userTempSecond = users.stream()
-                .filter(user -> user.getId() == toId)
-                .findFirst()
-                .get();
+        User2 userTempFirst = users.get(fromId);
+        User2 userTempSecond = users.get(toId);
         userTempFirst.setAmount(userTempFirst.getAmount() - amount);
         userTempSecond.setAmount(userTempSecond.getAmount() + amount);
         update(userTempFirst);
