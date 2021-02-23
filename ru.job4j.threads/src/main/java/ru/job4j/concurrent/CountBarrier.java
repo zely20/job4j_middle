951b17ddecd9e4e422ddb4f9d861ba1bf4d8c93f
Aliaksandr Zelianko <zely20@mail.ru>
2021-02-23 12:39:31 +0300

    refactoring CountBarrier.java

--- ru.job4j.threads/src/main/java/ru/job4j/concurrent/CountBarrier.java
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/CountBarrier.java
@@ -2,22 +2,21 @@ package ru.job4j.concurrent;
 
 public class CountBarrier {
     private final Object monitor = this;
-
     private final int total;
 
     private int count = 0;
 
-    public CountBarrier(final int total) {
+    public  CountBarrier(final int total) {
         this.total = total;
     }
 
-    public void count() {
+    public synchronized void count() {
         for(int i = 0; i < total; i++){
             count++;
         }
     }
 
-    public void await() throws InterruptedException {
+    public synchronized void await() throws InterruptedException {
         if(count == total){
             monitor.notifyAll();
         } else {
