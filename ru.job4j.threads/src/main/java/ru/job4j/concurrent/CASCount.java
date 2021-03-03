db8861a48d6e160c6c6ee3035cf71c2e06a1cd6b
Aliaksandr Zelianko <zely20@mail.ru>
2021-03-03 13:24:07 +0300

    0. CAS - операции[6859#471962]

---
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/CASCount.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+    @ThreadSafe
+    public class CASCount {
+        private final AtomicReference<Integer> count = new AtomicReference<>();
+
+        public void increment() {
+            int v;
+            do {
+                v = count.get();
+            } while (!count.compareAndSet(v, v++));
+        }
+
+        public int get() {
+            return count.get();
+        }
+}
