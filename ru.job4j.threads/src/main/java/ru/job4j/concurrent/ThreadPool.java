1d1aa7c9a02b892eff39a1da5e5a4c00548a1f5c
Aliaksandr Zelianko <zely20@mail.ru>
2021-03-15 12:10:06 +0300

    add EmailNotification

--- ru.job4j.threads/src/main/java/ru/job4j/concurrent/ThreadPool.java
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/ThreadPool.java
@@ -14,37 +14,39 @@ public class ThreadPool {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        while (isRunning) {
-            try {
-                Thread thread = new Thread(tasks.poll());
-                thread.start();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+
+        try {
+            Thread thread = new Thread(tasks.poll());
+            thread.start();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
     }
 
     public void shutdown() {
         isRunning = false;
     }
 }
+
 class Main2 {
     public static void main(String[] args) {
         ThreadPool pool = new ThreadPool();
 
         Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 5; i++){
+            for (int i = 0; i < 5; i++) {
                 System.out.println(i);
             }
         });
         Thread thread2 = new Thread(() -> {
-            for (int i = 7; i < 12; i++){
+            for (int i = 7; i < 12; i++) {
                 System.out.println(i);
             }
         });
 
         pool.work(thread1);
         pool.work(thread2);
-
+        pool.shutdown();
     }
 }
+
