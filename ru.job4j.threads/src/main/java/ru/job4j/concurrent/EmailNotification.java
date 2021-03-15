cf6521c49dfccc95268ceadefeb7b36ba3f63951
Aliaksandr Zelianko <zely20@mail.ru>
2021-03-15 12:23:51 +0300

    2. ExecutorService рассылка почты.[63097#471928]

--- ru.job4j.threads/src/main/java/ru/job4j/concurrent/EmailNotification.java
+++ ru.job4j.threads/src/main/java/ru/job4j/concurrent/EmailNotification.java
@@ -1,4 +1,47 @@
 package ru.job4j.concurrent;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class EmailNotification {
+
+    ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(UserEmail user) {
+        String subject = "Notification " + user.getName() + " to email " + user.getEmail() +".";
+        String body = "Add a new event to " + user.getName();
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+                send(subject, body, user.getEmail());
+            }
+        });
+    }
+
+    public void close(){
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email){}
+}
+class UserEmail {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
