ec27abba48f5dc4749ab9955b75968740bc35daa
Aliaksandr Zelianko <zely20@mail.ru>
2021-07-30 12:08:56 +0300

    add reg

--- ru.job4j.threads/src/main/java/ru/job4j/spring/SpringDI.java
+++ ru.job4j.threads/src/main/java/ru/job4j/spring/SpringDI.java
@@ -7,6 +7,7 @@ public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(Store.class);
+        context.register(ConsoleInput.class);
         context.register(StartUI.class);
         context.refresh();
         StartUI ui = context.getBean(StartUI.class);
